63fe394e39450732740564371191b296a0ca7665
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-14 00:30:14 +0300

    first IDEA commit

---
+++ src/main/java/org/skypro/starbank/marketing/StarBankMarketingApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.starbank.marketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBankMarketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarBankMarketingApplication.class, args);
+	}
+
+}
