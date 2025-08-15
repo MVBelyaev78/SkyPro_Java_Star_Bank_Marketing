9b8b382aff238a26e4cbaca531f4b6763db44e60
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-16 02:43:32 +0300

    application skeleton modified (in progress)

--- src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
+++ src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
@@ -3,12 +3,14 @@ package org.skypro.starbank.marketing.controller;
 import org.skypro.starbank.marketing.model.recommendation.Recommendation;
 import org.skypro.starbank.marketing.service.RecommendationService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
 
