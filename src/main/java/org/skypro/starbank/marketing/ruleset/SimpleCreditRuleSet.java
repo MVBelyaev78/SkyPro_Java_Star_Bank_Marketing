b4ee1562faf094f492a829b45902993e06073882
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-16 02:52:38 +0300

    application skeleton modified (in progress)

--- src/main/java/org/skypro/starbank/marketing/ruleset/SimpleCreditRuleSet.java
+++ src/main/java/org/skypro/starbank/marketing/ruleset/SimpleCreditRuleSet.java
@@ -1,10 +1,12 @@
 package org.skypro.starbank.marketing.ruleset;
 
 import org.skypro.starbank.marketing.model.recommendation.Recommendation;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Component
 public class SimpleCreditRuleSet implements RecommendationRuleSet{
     @Override
     public Optional<Recommendation> getRecommendation(UUID userID) {
