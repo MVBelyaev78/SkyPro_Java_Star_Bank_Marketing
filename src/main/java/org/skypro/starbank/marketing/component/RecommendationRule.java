f8a5d1fef5d9a0fb7e17032fe282a0eb1ad4cfd5
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-17 14:31:58 +0300

    interface RecommendationRule added

---
+++ src/main/java/org/skypro/starbank/marketing/component/RecommendationRule.java
@@ -0,0 +1,10 @@
+package org.skypro.starbank.marketing.component;
+
+import org.skypro.starbank.marketing.dto.Recommendation;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RecommendationRule {
+    Optional<Recommendation> getRecommendation(UUID recommendationId);
+}
