cde20f1406face3a892a98b401c9b36de487fd41
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-17 14:36:01 +0300

    interface RecommendationRule updated

--- src/main/java/org/skypro/starbank/marketing/component/RecommendationRule.java
+++ src/main/java/org/skypro/starbank/marketing/component/RecommendationRule.java
@@ -6,5 +6,5 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface RecommendationRule {
-    Optional<Recommendation> getRecommendation(UUID recommendationId);
+    Optional<Recommendation> getRecommendation(UUID userId);
 }
