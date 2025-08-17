a533e5d75abc527d1cd3ae83c63715df2f8c5000
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-17 14:23:25 +0300

    method getServiceResult updated

--- src/main/java/org/skypro/starbank/marketing/service/RecommendationService.java
+++ src/main/java/org/skypro/starbank/marketing/service/RecommendationService.java
@@ -11,13 +11,13 @@ import java.util.UUID;
 @Service
 public class RecommendationService {
     public ServiceResult getServiceResult(String userId) {
-        final Collection<Recommendation> serviceResult = new ArrayList<>();
-        serviceResult.add(new Recommendation("Yesterday",
+        final Collection<Recommendation> recommendations = new ArrayList<>();
+        recommendations.add(new Recommendation("Yesterday",
                 UUID.fromString("b9f1a8b5-35c5-4372-a83f-6f61e53c5c1a"),
                 "I said something wrong, now I long for yesterday"));
-        serviceResult.add(new Recommendation("Thank you for the music",
+        recommendations.add(new Recommendation("Thank you for the music",
                 UUID.fromString("65545241-06e3-4a1d-b7a5-8b0a7f9aa4e8"),
                 "She says I began to sing long before I could talk"));
-        return new ServiceResult(UUID.fromString(userId), serviceResult);
+        return new ServiceResult(UUID.fromString(userId), recommendations);
     }
 }
