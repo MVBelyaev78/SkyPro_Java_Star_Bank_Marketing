07cf50dcc08161e16c118d865cb7607acb320309
GockinD <gockindmitrij0@gmail.com>
2025-08-22 16:11:22 +0500

    A REST API document in Swagger format

--- src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
+++ src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
@@ -1,5 +1,9 @@
 package org.skypro.starbank.marketing.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.skypro.starbank.marketing.service.RecommendationService;
 import org.skypro.starbank.marketing.result.ServiceResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
+@Tag(name = "Recommendation API", description = "Операции для получения персональных рекомендаций")
 @RequestMapping("/recommendation")
 public class RecommendationController {
     private final RecommendationService recommendationService;
@@ -19,7 +24,19 @@ public class RecommendationController {
     }
 
     @GetMapping("/{userId}")
-    public ServiceResult getRecommendationList(@PathVariable String userId) {
+    @Operation(
+            summary = "Получить список рекомендаций для пользователя",
+            description = "Возвращает персонализированный список продуктов банка, подобранный на основе ID пользователя"
+    )
+    @ApiResponse(responseCode = "200", description = "Успешный запрос, рекомендации найдены")
+    @ApiResponse(responseCode = "404", description = "Пользователь с указанным ID не найден")
+    public ServiceResult getRecommendationList(
+            @Parameter(
+            description = "Уникальный идентификатор пользователя в системе банка",
+            required = true,
+            example = "f37ba8a8-3cd5-4976-9f74-2b21f105da67"
+    )
+            @PathVariable String userId) {
         return recommendationService.getServiceResult(UUID.fromString(userId));
     }
 }
--- src/main/java/org/skypro/starbank/marketing/result/ServiceResult.java
+++ src/main/java/org/skypro/starbank/marketing/result/ServiceResult.java
@@ -1,13 +1,18 @@
 package org.skypro.starbank.marketing.result;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.skypro.starbank.marketing.dto.Recommendation;
 
 import java.util.Collection;
 import java.util.UUID;
 
 public class ServiceResult {
+    @Schema(description = "UUID пользователя, для которого подобраны рекомендации",
+            example = "f37ba8a8-3cd5-4976-9f74-2b21f105da67")
     private final UUID userId;
+
+    @Schema(description = "Список рекомендованных финансовых продуктов")
     private final Collection<Recommendation> recommendations;
 
     public ServiceResult(UUID userId, Collection<Recommendation> recommendations) {
