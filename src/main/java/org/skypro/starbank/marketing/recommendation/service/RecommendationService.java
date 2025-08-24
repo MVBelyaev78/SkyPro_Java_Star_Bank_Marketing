61134a4d6254512a566ce451bea03fce1a7020b2
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-24 12:18:58 +0300

    (in progress)

--- src/main/java/org/skypro/starbank/marketing/recommendation/service/RecommendationService.java
+++ src/main/java/org/skypro/starbank/marketing/recommendation/service/RecommendationService.java
@@ -1,5 +1,8 @@
 package org.skypro.starbank.marketing.recommendation.service;
 
+import org.skypro.starbank.marketing.dynamicrule.configuration.DynamicRulesDatabase;
+import org.skypro.starbank.marketing.dynamicrule.configuration.DynamicRulesDatabaseEmulator;
+import org.skypro.starbank.marketing.dynamicrule.dto.DynamicRule;
 import org.skypro.starbank.marketing.recommendation.component.RecommendationRule;
 import org.skypro.starbank.marketing.recommendation.dto.Recommendation;
 import org.skypro.starbank.marketing.recommendation.dto.RecommendationServiceResult;
@@ -21,6 +24,10 @@ public class RecommendationService {
         final Collection<Recommendation> recommendations = new HashSet<>();
         rules.forEach(rule -> rule.getRecommendation(userId)
                 .ifPresent(recommendations::add));
+        DynamicRulesDatabase dynamicRulesDB = new DynamicRulesDatabaseEmulator();
+        /*for (DynamicRule dynamicRule : dynamicRulesDB.getRules()) {
+
+        }*/
         return new RecommendationServiceResult(userId, recommendations);
     }
 }
