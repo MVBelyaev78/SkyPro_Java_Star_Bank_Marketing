da1f0246628fc46c762c3eed48b6847e309a250f
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-25 19:33:17 +0300

    Conflicts eliminated

--- src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
+++ src/main/java/org/skypro/starbank/marketing/controller/RecommendationController.java
@@ -1,13 +1,14 @@
 package org.skypro.starbank.marketing.controller;
 
 import org.skypro.starbank.marketing.service.RecommendationService;
-import org.skypro.starbank.marketing.result.ServiceResult;
 import org.springframework.cache.annotation.Cacheable;
 import org.skypro.starbank.marketing.dto.recommendation.RecommendationServiceResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 
 @RestController
@@ -21,8 +22,8 @@ public class RecommendationController {
 
     @GetMapping("/{userId}")
     @Cacheable(value = "recommendationsCache", key = "#userId")
-    public ServiceResult getRecommendationList(@PathVariable String userId) {
-        return recommendationService.getServiceResult(userId);
+    public RecommendationServiceResult getRecommendationList(@PathVariable String userId) {
+        return recommendationService.getServiceResult(UUID.fromString(userId));
     }
 
 }
--- src/main/java/org/skypro/starbank/marketing/service/RecommendationService.java
+++ src/main/java/org/skypro/starbank/marketing/service/RecommendationService.java
@@ -4,13 +4,9 @@ import org.skypro.starbank.marketing.auxiliary.NewCollection;
 import org.skypro.starbank.marketing.component.recommendation.collect.RecommendationCollect;
 import org.skypro.starbank.marketing.dto.recommendation.Recommendation;
 import org.skypro.starbank.marketing.dto.recommendation.RecommendationServiceResult;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.skypro.starbank.marketing.dto.Recommendation;
-import org.skypro.starbank.marketing.result.ServiceResult;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
 
@@ -18,21 +14,11 @@ import java.util.UUID;
 public class RecommendationService {
     private final Collection<RecommendationCollect> recommendationCollects;
 
-    @Autowired
     public RecommendationService(Collection<RecommendationCollect> recommendationCollects) {
         this.recommendationCollects = recommendationCollects;
-    @Cacheable(value = "recommendationsCache", key = "#userId")
-    public ServiceResult getServiceResult(String userId) {
-        final Collection<Recommendation> serviceResult = new ArrayList<>();
-        serviceResult.add(new Recommendation("Yesterday",
-                UUID.fromString("b9f1a8b5-35c5-4372-a83f-6f61e53c5c1a"),
-                "I said something wrong, now I long for yesterday"));
-        serviceResult.add(new Recommendation("Thank you for the music",
-                UUID.fromString("65545241-06e3-4a1d-b7a5-8b0a7f9aa4e8"),
-                "She says I began to sing long before I could talk"));
-        return new ServiceResult(UUID.fromString(userId), serviceResult);
     }
 
+    @Cacheable(value = "recommendationsCache", key = "#userId")
     public RecommendationServiceResult getServiceResult(UUID userId) {
         final Collection<Recommendation> recResult = new NewCollection<Recommendation>().initCollection();
         recommendationCollects
