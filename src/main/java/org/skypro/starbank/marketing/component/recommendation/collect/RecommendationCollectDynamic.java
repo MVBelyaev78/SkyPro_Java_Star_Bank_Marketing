a3734c40672b8684cbd3403b1b47f2ff3bc5dcbe
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-26 17:34:07 +0300

    refactoring

--- src/main/java/org/skypro/starbank/marketing/component/recommendation/collect/RecommendationCollectDynamic.java
+++ src/main/java/org/skypro/starbank/marketing/component/recommendation/collect/RecommendationCollectDynamic.java
@@ -3,7 +3,6 @@ package org.skypro.starbank.marketing.component.recommendation.collect;
 import org.skypro.starbank.marketing.auxiliary.NewCollection;
 import org.skypro.starbank.marketing.component.dynamicrule.DynamicRecommendationRules;
 import org.skypro.starbank.marketing.configuration.dynamicrule.DynamicRulesDatabase;
-import org.skypro.starbank.marketing.configuration.dynamicrule.DynamicRulesDatabaseEmulator;
 import org.skypro.starbank.marketing.dto.recommendation.Recommendation;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +12,17 @@ import java.util.UUID;
 @Component
 public class RecommendationCollectDynamic implements RecommendationCollect {
     private final DynamicRecommendationRules dynRecRules;
+    private final DynamicRulesDatabase dynamicRulesDatabase;
 
-    public RecommendationCollectDynamic(DynamicRecommendationRules dynRecRules) {
+    public RecommendationCollectDynamic(DynamicRecommendationRules dynRecRules, DynamicRulesDatabase dynamicRulesDatabase) {
         this.dynRecRules = dynRecRules;
+        this.dynamicRulesDatabase = dynamicRulesDatabase;
     }
 
     @Override
     public Collection<Recommendation> getRecommendations(UUID userId) {
         final Collection<Recommendation> recommendations = new NewCollection<Recommendation>().initCollection();
-        final DynamicRulesDatabase dynamicRulesDB = new DynamicRulesDatabaseEmulator();
-        dynamicRulesDB
+        dynamicRulesDatabase
                 .getRules()
                 .stream()
                 .filter(dynamicRule -> dynRecRules
