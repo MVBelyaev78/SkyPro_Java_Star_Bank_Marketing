7a4cdfebddf4207102a0351e1ce3239e83697bfa
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-26 13:15:15 +0300

    refactoring

--- src/main/java/org/skypro/starbank/marketing/dto/recommendation/Recommendation.java
+++ src/main/java/org/skypro/starbank/marketing/dto/recommendation/Recommendation.java
@@ -5,11 +5,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.util.UUID;
 
-public record Recommendation(@Schema(description = "Название финансового продукта",
-        example = "Вклад 'Копилка'") String name, @Schema(description = "Уникальный идентификатор рекомендации",
-        example = "59efc529-2fff-41af-baff-90ccd7402925") UUID uuid,
-                             @Schema(description = "Детальное описание предложения и его условий",
-                                     example = "Откройте свою собственную «Копилку» с нашим банком! «Копилка» — это уникальный банковский инструмент, который поможет вам легко и удобно накапливать деньги на важные цели.") String text) {
+public record Recommendation(
+        @Schema(description = "Название финансового продукта",
+                example = "Вклад 'Копилка'")
+            String name,
+        @Schema(description = "Уникальный идентификатор рекомендации",
+                example = "59efc529-2fff-41af-baff-90ccd7402925")
+            UUID uuid,
+        @Schema(description = "Детальное описание предложения и его условий",
+                example = "Откройте свою собственную «Копилку» с нашим банком! «Копилка» — это уникальный банковский инструмент, который поможет вам легко и удобно накапливать деньги на важные цели.")
+            String text) {
     public Recommendation(String name, UUID uuid, String text) {
         this.name = name;
         this.uuid = uuid;
