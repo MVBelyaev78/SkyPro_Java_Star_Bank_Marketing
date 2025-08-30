f653eb6ea97a7634ac29014cc2920523ff8d233e
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-08-31 01:07:44 +0300

    tiny refactoring

--- src/main/java/org/skypro/starbank/marketing/dto/dynamicrule/DynamicRule.java
+++ src/main/java/org/skypro/starbank/marketing/dto/dynamicrule/DynamicRule.java
@@ -8,11 +8,20 @@ import java.util.UUID;
 
 @Schema(description = "Динамическое правило для генерации рекомендаций")
 public record DynamicRule(
-        @Schema(hidden = true) UUID id,
-        @Schema(description = "Название финансового продукта", example = "Ипотека 'Молодая семья'", requiredMode = Schema.RequiredMode.REQUIRED) String name,
-        @Schema(description = "UUID рекомендуемого продукта", example = "f56b9e7e-eb9a-4f27-9c3a-910a2bb8f7f1", requiredMode = Schema.RequiredMode.REQUIRED) UUID recommendationUuid,
-        @Schema(description = "Описание продукта", example = "Специальная программа ипотечного кредитования для молодых семей.") String text,
-        @Schema(description = "Список условий правила", requiredMode = Schema.RequiredMode.REQUIRED) Collection<QueryType> rule) {
+        @Schema(hidden = true)
+        UUID id,
+
+        @Schema(description = "Название финансового продукта", example = "Ипотека 'Молодая семья'", requiredMode = Schema.RequiredMode.REQUIRED)
+        String name,
+
+        @Schema(description = "UUID рекомендуемого продукта", example = "f56b9e7e-eb9a-4f27-9c3a-910a2bb8f7f1", requiredMode = Schema.RequiredMode.REQUIRED)
+        UUID recommendationUuid,
+
+        @Schema(description = "Описание продукта", example = "Специальная программа ипотечного кредитования для молодых семей.")
+        String text,
+
+        @Schema(description = "Список условий правила", requiredMode = Schema.RequiredMode.REQUIRED)
+        Collection<QueryType> rule) {
     public DynamicRule(UUID id, String name, UUID recommendationUuid, String text, Collection<QueryType> rule) {
         this.id = id;
         this.name = name;
