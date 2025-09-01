1d8b9dc299693f04d5aa920c66408001e0435b07
Mikhail Belyaev <mbelyaev78@gmail.com>
2025-09-01 21:02:16 +0300

    refactoring

--- src/main/java/org/skypro/starbank/marketing/controller/ManagementController.java
+++ src/main/java/org/skypro/starbank/marketing/controller/ManagementController.java
@@ -2,7 +2,7 @@ package org.skypro.starbank.marketing.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.cache.CacheManager;
+import org.skypro.starbank.marketing.service.ManagementService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,21 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/management")
 @Tag(name = "Management Controller", description = "Служебный API")
 public class ManagementController {
-    private final CacheManager cacheManager;
+    private final ManagementService managementService;
 
-    public ManagementController(CacheManager cacheManager) {
-        this.cacheManager = cacheManager;
+    public ManagementController(ManagementService managementService) {
+        this.managementService = managementService;
     }
 
     @PostMapping("/clear-caches")
-    @Operation(summary = "Очистка всех кэшей", description = "Полностью очищает все закэшированные результаты рекомендаций")
+    @Operation(summary = "Очистка всех кэшей",
+            description = "Полностью очищает все закэшированные результаты рекомендаций")
     public ResponseEntity<Void> clearAllCaches() {
-        cacheManager.getCacheNames().forEach(cacheName -> {
-            var cache = cacheManager.getCache(cacheName);
-            if (cache != null) {
-                cache.clear();
-            }
-        });
+        managementService.clearAllCaches();
         return ResponseEntity.ok().build();
     }
 }
